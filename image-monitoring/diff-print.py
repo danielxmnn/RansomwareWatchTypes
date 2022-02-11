d1a0eb1e835724653f1563333becde36e106b808
danielxmnn <58004682+danielxmnn@users.noreply.github.com>
2022-02-10 23:20:29 -0300

    1

---
+++ image-monitoring/diff-print.py
@@ -0,0 +1,42 @@
+from time import sleep
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import index
+import diferenca
+
+aux = index.lista
+lista = list(aux.keys())
+SevErro = "Error accessing resource: "
+SevErro2 = "Onion.ws is a "
+
+
+def _screenshot(a):
+    driver = webdriver.Firefox()
+    driver.get(a)
+    sleep(15)
+    el = driver.find_element(By.NAME, 'body')
+    el.screenshot("img/" + str(x) + ".png")
+    driver.close()
+    driver.quit()
+    return
+
+
+try:
+    for x in lista:
+        url = aux[x]
+        site = url["site"]
+        try:
+            site = url["site"]
+            _screenshot(site)
+            diff = diferenca.diff("img/"+site+".png", "img/erro.png")
+            if diff > 0.001:
+                print(diff)
+        except Exception as e:
+            print("Error: ")
+            print(e)
+            pass
+
+except Exception as e:
+    print("Error: ")
+    print(e)
