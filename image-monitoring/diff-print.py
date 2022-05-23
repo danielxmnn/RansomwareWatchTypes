502cc296148d1c958edcefa0d1406916d5dd28e8
danielxmnn <58004682+danielxmnn@users.noreply.github.com>
2022-05-23 18:16:38 -0300

    1.3

--- image-monitoring/diff-print.py
+++ image-monitoring/diff-print.py
@@ -1,34 +1,30 @@
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from html2image import Html2Image
 import index
 import diferenca
+import requests
 
 aux = index.lista
 lista = list(aux.keys())
-
-
-def _screenshot(a):
-    driver = webdriver.Firefox()
-    driver.get(a)
-    sleep(15)
-    el = driver.find_element(By.NAME, 'body')
-    el.screenshot("img/" + str(x) + ".png")
-    driver.close()
-    driver.quit()
-    return
-
+hti = Html2Image(custom_flags=['--virtual-time-budget=15000', '--hide-scrollbars'])
+hti.size = (1500, 3200)
+hti.output_path = 'img'
+SevErro = "is a darknet gateway or Tor2Web"
 
 try:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/96.0.4664.110 Safari/537.36'
+    }
     for x in lista:
         url = aux[x]
         site = url["site"]
         try:
-            site = url["site"]
-            _screenshot(site)
-            diff = diferenca.diff("img/"+site+".png", "img/erro.png")
-            if diff > 0.001:
-                print(diff)
+            response = requests.get(site, headers=headers)
+            if SevErro not in response.text:
+                hti.screenshot(url=site, save_as=str(x) + ".png")
+                diff = diferenca.diff("img/"+x+".png", "img/"+x+"_base.png")
+                if diff > 0.001:
+                    print(diff)
         except Exception as e:
             print("Error: ")
             print(e)
